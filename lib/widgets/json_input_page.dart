d119f14cbda5c26dfc632aebf2ca561d1c36e64c
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2024-02-22 09:42:47 +0300

    chore: add horizontal table

--- lib/widgets/json_input_page.dart
+++ lib/widgets/json_input_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:insights_app/main.dart';
 import 'package:insights_app/widgets/json_input_widget.dart';
 
 class JsonInputPage extends StatelessWidget {
