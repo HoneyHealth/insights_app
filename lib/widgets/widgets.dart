1c80c8bd06fc4a990be6c9cfecf4e1e16a2b39d8
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2024-03-11 18:28:03 +0300

    feat: use the insight filter card

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -4,4 +4,4 @@ export 'exported_json_page.dart';
 export 'insight/insight_widget.dart';
 export 'json_input_page.dart';
 export 'json_input_widget.dart';
-export 'user_selection_page.dart';
+export 'user_selection_page/user_selection_page.dart';
